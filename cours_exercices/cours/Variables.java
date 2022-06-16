48ae3959a88eb0f427bdc4ebd00f83e6a4208a49
Twixy Twixy <twixy@free.fr>
2022-06-16 17:17:49 +0200

    Cours_Exercices

---
+++ cours_exercices/cours/Variables.java
@@ -0,0 +1,58 @@
+/**
+ * Exemple sur les variables
+ */
+
+package cours_exercices.cours;
+
+// Classe principale
+public class Variables {
+    public static void main(String[] args) {
+        // Opérateur d'affectation Simple
+        
+        // Affichage simple d'une variable
+        
+        // Affectation multiple
+        
+        // Boxing
+        
+        // Unboxing
+
+        // Casting
+
+        // Conversion d'une String en classe Integer
+
+        // Conversion d'une String en int primitif
+
+        // Conversion d'un double en String
+
+        // Opérateur arithmétique simple
+
+        // Opérateur unitaire postfixé
+
+        // Opérateur unitaire préfixé
+
+        // Opérateur binaire
+
+        // Comparaison avec des chaines de caractères 
+
+        // Opérateur de comparaison logique
+
+        // Opérateur de négation
+
+        // Opérateur avec assignation
+        
+        // Saisir au clavier et affecter la saisie à une variable
+
+        // Récupérer la ligne saisie
+
+        // Récupérer un caractère
+        
+        // Récupérer un entier
+
+        // Récupérer un nombre à virgule (ne pas saisir un point)
+        
+        // Récupérer un boolean (saisir true ou false seulement)
+
+        // Affichage des variables
+    }
+}
---
+++ cours_exercices/exercices/conditions/ExConditions.java
@@ -0,0 +1,31 @@
+/*
+ * Exercice conditions
+ * 
+ * 1. Chercher le plus grand des 3 nombres saisie par l'utilisateur
+ * 2. Afficher sur les nombres saisie sont pairs ou impairs
+ * 3. Demander à l'utilisateur de saisir le numéro d'un mois de l'année
+ * et afficher le nombre de jours correspondant au mois choisi (28/29) pour février
+ * indiquer un message d'erreur si le numéro de mois n'est pas compris entre 1 et 12.
+ */
+package cours_exercices.exercices.conditions;
+import java.util.Scanner;
+        
+public class ExConditions {
+    public static void main(String[] args) {
+        // Déclarations / définitions des variables
+         
+        // Fournir les données d'entrée
+ 
+        // Chercher le maximum
+ 
+        // Afficher le résultat
+ 
+        // Afficher si num1 est pair ou impair
+     
+        // Fournir les données d'entrée
+ 
+        // Afficher le résultat
+        
+        // Fermer les ressources
+    }
+}
