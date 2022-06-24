da8c46e29068064fae87a2d473de35afbdfe426f
FI86 <105541108+FI86@users.noreply.github.com>
2022-06-24 16:57:53 +0200

    changement correction exercice heritage

--- corriges/exercices/hertiage/ExHeritage.java
+++ corriges/exercices/hertiage/ExHeritage.java
@@ -10,11 +10,11 @@
  * Vous y ajouterez une méthode volume qui devra renvoyer le volume du cylindre
  * (rappel : volume d’un cylindre = surface de section × hauteur).
  */
-package corriges.exercices.hertiage;
+package cours_exercices.exercices.hertiage;
 
 // Classe Cercle
 class Cercle {
-    // propriété
+    // Propriété
     private int rayon;
     
     // Constructeur
@@ -24,9 +24,9 @@ class Cercle {
     
     // Getters
     public int getRayon() {
-        return rayon;
+        return this.rayon;
     }
-
+    
     // Setters
     public void setRayon(int rayon) {
         this.rayon = rayon;
@@ -34,38 +34,38 @@ class Cercle {
     
     // Methodes
     public double perimetre() {
-        return 2 * Math.PI * rayon;
+        return Math.PI * 2 * this.rayon;
     }
     
     public double surface() {
-        return Math.PI * rayon * rayon;
+        return Math.PI * this.rayon * this.rayon;
     }
 }
 
 // Classe Cylindre
 class Cylindre extends Cercle {
-    // propriété
+    // Propriété
     private float hauteur;
     
     // Constructeur
     public Cylindre(int rayon, float hauteur) {
         super(rayon);
-        this.setHauteur(hauteur);
+        this.hauteur = hauteur;
     }
     
     // Getters
-    public float getHauteur() {    
-        return hauteur;
+    public float getHauteur() {
+        return this.hauteur;
     }
-
+    
     // Setters
-    public void setHauteur(float hauteur) {    
+    public void setHauteur(float hauteur) {
         this.hauteur = hauteur;
     }
-
+    
     // Methode
     public double volume() {
-        return this.surface() * this.hauteur;
+        return super.surface() * this.hauteur;
     }
 }
 
@@ -75,15 +75,15 @@ public class ExHeritage {
         Cercle ce = new Cercle(15);
         Cylindre cy = new Cylindre(10, 11);
         
-        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour perimetre : " + ce.perimetre());
-        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour surface : " + ce.surface());
-        System.out.println("Cylindre de rayon " + cy.getRayon() + " et de hauteur " + cy.getHauteur() + " a pour volume : " + cy.volume());
+        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour perimetre " + ce.perimetre());
+        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour surface " + ce.surface());
+        System.out.println("Cylindre de rayon " + cy.getRayon() + " et de hauteur " + cy.getHauteur() + " a pour volume " + cy.volume());
         
         // avec arrondi
         System.out.println();
-        System.out.println("Avec valeur arrondie :");
-        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour perimetre : " + Math.round(ce.perimetre() * 100) / 100.0);
-        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour surface : " + Math.round(ce.surface() * 100) / 100.0);
-        System.out.println("Cylindre de rayon " + cy.getRayon() + " et de hauteur " + cy.getHauteur() + " a pour volume : " + Math.round(cy.volume() * 100) / 100.0);
+        System.out.println("Avec arrondie : ");
+        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour perimetre " + Math.round(ce.perimetre() *100) / 100.0);
+        System.out.println("Cercle de rayon " + ce.getRayon() + " a pour surface " + Math.round(ce.surface() * 100) / 100.0);
+        System.out.println("Cylindre de rayon " + cy.getRayon() + " et de hauteur " + cy.getHauteur() + " a pour volume " + Math.round(cy.volume() * 100) / 100.0);
     }
 }
