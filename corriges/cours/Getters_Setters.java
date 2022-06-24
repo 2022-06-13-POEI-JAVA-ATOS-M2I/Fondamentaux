48b1d9ffbbf29702fbe3fd90ce87ccd255332606
FI86 <105541108+FI86@users.noreply.github.com>
2022-06-24 10:25:57 +0200

    corriges cours getters-setters

---
+++ corriges/cours/Getters_Setters.java
@@ -0,0 +1,24 @@
+package cours_exercices.cours;
+
+class Velo{
+    private int nbRoues = 2;
+    
+    public int getNbRoues() {
+        return this.nbRoues;
+    }
+    
+    public void setNbRoues(int nbRoues) {
+        this.nbRoues = nbRoues;
+    }
+}
+
+
+public class Getters_Setters {
+    public static void main(String[] args) {
+        Velo v = new Velo();
+        
+        System.out.println(v.getNbRoues());
+        v.setNbRoues(3);
+        System.out.println(v.getNbRoues());
+    }
+}
